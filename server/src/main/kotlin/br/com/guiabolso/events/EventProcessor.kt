8e90bb0821145a0a8718d1da3f8f053df951c216
José Almada <jose.victor@guiabolso.com.br>
2017-09-27 15:40:18 -0300

    Event processor using the same mapper as the core

--- server/src/main/kotlin/br/com/guiabolso/events/EventProcessor.kt
+++ server/src/main/kotlin/br/com/guiabolso/events/EventProcessor.kt
@@ -5,13 +5,13 @@ import br.com.guiabolso.events.builder.EventBuilder.Companion.eventNotFound
 import br.com.guiabolso.events.exception.EventExceptionHandler
 import br.com.guiabolso.events.exception.ExceptionHandlerRegistry
 import br.com.guiabolso.events.handler.EventHandlerDiscovery
+import br.com.guiabolso.events.json.MapperHolder
 import br.com.guiabolso.events.metric.CompositeMetricReporter
 import br.com.guiabolso.events.metric.MDCMetricReporter
 import br.com.guiabolso.events.metric.MetricReporter
 import br.com.guiabolso.events.metric.NewrelicMetricReporter
 import br.com.guiabolso.events.model.*
 import br.com.guiabolso.events.validation.EventValidator.validateAsRequestEvent
-import com.google.gson.Gson
 import org.apache.commons.lang3.exception.ExceptionUtils.getStackTrace
 import org.slf4j.LoggerFactory.getLogger
 
@@ -22,7 +22,6 @@ class EventProcessor(
 
     companion object {
         private val logger = getLogger(EventProcessor::class.java)!!
-        private val mapper = Gson()
     }
 
     fun <T : Throwable> register(clazz: Class<T>, handler: EventExceptionHandler<T>) {
@@ -54,7 +53,7 @@ class EventProcessor(
 
     private fun parseAndValidateEvent(rawEvent: String): Event =
             try {
-                val input = mapper.fromJson(rawEvent, RawEvent::class.java)
+                val input = MapperHolder.mapper.fromJson(rawEvent, RawEvent::class.java)
                 validateAsRequestEvent(input)
             } catch (e: IllegalArgumentException) {
                 logger.error("Missing required property ${e.message}.", e)
