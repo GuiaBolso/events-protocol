896cb0b7c2f95265cbd34a96d8d4b0658a24c8ab
Bruno Ortiz <brunortiz11@gmail.com>
2017-09-27 19:01:05 -0300

    ExceptionHandlerRegistry tests

---
+++ server/src/test/kotlin/br/com/guiabolso/events/ExceptionHandlerRegistryTest.kt
@@ -0,0 +1,67 @@
+package br.com.guiabolso.events
+
+import br.com.guiabolso.events.exception.EventExceptionHandler
+import br.com.guiabolso.events.exception.ExceptionHandlerRegistry
+import br.com.guiabolso.events.metric.MetricReporter
+import br.com.guiabolso.events.model.Event
+import br.com.guiabolso.events.model.ResponseEvent
+import com.google.gson.JsonPrimitive
+import org.junit.Assert
+import org.junit.Test
+
+class ExceptionHandlerRegistryTest {
+
+
+    @Test
+    fun testCanRegisterExceptionHandler() {
+        val exceptionHandlerRegistry = ExceptionHandlerRegistry()
+
+        exceptionHandlerRegistry.register(RuntimeException::class.java, object : EventExceptionHandler<RuntimeException> {
+
+            override fun handleException(exception: RuntimeException, event: Event, metricReporter: MetricReporter): ResponseEvent {
+                return EventBuilderForTest.buildResponseEvent().copy(payload = JsonPrimitive(exception.message))
+            }
+
+        })
+
+        val response = exceptionHandlerRegistry.handleException(
+                RuntimeException("Some error"),
+                EventBuilderForTest.buildRequestEvent(),
+                NoOpMetricReporter())
+
+        Assert.assertEquals("Some error", response.payload.asString)
+    }
+
+
+    @Test
+    fun testHandleDefaultError() {
+        val exceptionHandlerRegistry = ExceptionHandlerRegistry()
+
+        val response = exceptionHandlerRegistry.handleException(
+                RuntimeException("Some error"),
+                EventBuilderForTest.buildRequestEvent(),
+                NoOpMetricReporter())
+
+        Assert.assertEquals("UNHANDLED_ERROR", response.payload.asJsonObject["code"].asString)
+    }
+}
+
+
+class NoOpMetricReporter : MetricReporter {
+    override fun startProcessingEvent(event: Event) {
+        //NOTHING
+    }
+
+    override fun eventProcessFinished(event: Event) {
+        //NOTHING
+    }
+
+    override fun addProperty(key: String, value: String) {
+        //NOTHING
+    }
+
+    override fun notifyError(exception: Throwable) {
+        //NOTHING
+    }
+
+}
