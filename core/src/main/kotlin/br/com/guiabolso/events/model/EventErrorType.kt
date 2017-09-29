1849d206c9ac5b11540dafdb46dac6db8f87606f
Bruno Ortiz <brunortiz11@gmail.com>
2017-09-29 16:49:29 -0300

    Adding more error types

--- core/src/main/kotlin/br/com/guiabolso/events/model/EventErrorType.kt
+++ core/src/main/kotlin/br/com/guiabolso/events/model/EventErrorType.kt
@@ -7,11 +7,15 @@ sealed class EventErrorType(open val typeName: String) {
         fun getErrorType(errorType: String) = when (errorType) {
             "error" -> Generic
             "notFound" -> NotFound
+            "badRequest" -> BadRequest
+            "unauthorized" -> Unauthorized
             else -> Unknown(errorType)
         }
     }
 
     object Generic : EventErrorType("error")
+    object BadRequest : EventErrorType("badRequest")
+    object Unauthorized : EventErrorType("unauthorized")
     object NotFound : EventErrorType("notFound")
     data class Unknown(override val typeName: String) : EventErrorType(typeName)
 
