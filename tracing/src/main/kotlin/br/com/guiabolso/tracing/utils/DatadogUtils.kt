be699a0fcfa58ed9b6bec04e821e9bf220ae4151
José Victor Almada <jose.victor@guiabolso.com.br>
2018-08-29 14:10:59 -0300

    Corrigindo compatibilidade com JVM 7 (#14)

--- tracing/src/main/kotlin/br/com/guiabolso/tracing/utils/DatadogUtils.kt
+++ tracing/src/main/kotlin/br/com/guiabolso/tracing/utils/DatadogUtils.kt
@@ -1,26 +1,59 @@
 package br.com.guiabolso.tracing.utils
 
-import datadog.trace.api.DDTags
+import datadog.trace.api.DDSpanTypes.WEB_SERVLET
+import datadog.trace.api.DDTags.ERROR_MSG
+import datadog.trace.api.DDTags.ERROR_STACK
+import datadog.trace.api.DDTags.ERROR_TYPE
+import datadog.trace.api.DDTags.SPAN_TYPE
+import io.opentracing.Span
 import io.opentracing.tag.Tags
 import io.opentracing.util.GlobalTracer
 
 
 object DatadogUtils {
 
-    fun traceOperation(name: String, func: () -> Any) {
+    @JvmStatic
+    @JvmOverloads
+    fun traceAsNewOperation(name: String, type: String = WEB_SERVLET, func: () -> Any) {
         val tracer = GlobalTracer.get()!!
         tracer.buildSpan(name).ignoreActiveSpan().startActive(true).use {
             try {
+                it.span().setTag(SPAN_TYPE, type)
                 func()
             } catch (e: Exception) {
-                tracer.activeSpan()?.apply {
-                    setTag(Tags.ERROR.key, true)
-                    setTag(DDTags.ERROR_MSG, e.message ?: "Empty message")
-                    setTag(DDTags.ERROR_TYPE, e.javaClass.name)
-                    setTag(DDTags.ERROR_STACK, ExceptionUtils.getStackTrace(e))
-                }
+                notifyError(it.span(), e, false)
             }
         }
+    }
+
+    @JvmStatic
+    fun <T> traceBlock(name: String, func: () -> T): T {
+        val tracer = GlobalTracer.get()!!
+
+        val scope = tracer.buildSpan(name).startActive(true)!!
+        return scope.use { _ ->
+            try {
+                func()
+            } catch (e: Exception) {
+                notifyError(scope.span(), e, false)
+                throw e
+            }
+        }
+    }
+
+    @JvmStatic
+    fun notifyError(span: Span, exception: Throwable, expected: Boolean) {
+        span.setTag(Tags.ERROR.key, (!expected).toString())
+        span.setTag(ERROR_MSG, exception.message ?: "Empty message")
+        span.setTag(ERROR_TYPE, exception.javaClass.name)
+        span.setTag(ERROR_STACK, ExceptionUtils.getStackTrace(exception))
+    }
+
+    @JvmStatic
+    fun notifyError(span: Span, message: String, params: Map<String, String?>, expected: Boolean) {
+        span.setTag(Tags.ERROR.key, (!expected).toString())
+        span.setTag(ERROR_MSG, message)
+        params.forEach { entry -> span.setTag(entry.key, entry.value) }
     }
 
 }
