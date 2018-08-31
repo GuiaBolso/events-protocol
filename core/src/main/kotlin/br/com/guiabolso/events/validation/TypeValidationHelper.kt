22c4d6a8ae286c05bd1e0224c53b15819804488e
chih.chen <chih.chen1029@gmail.com>
2018-08-31 11:45:03 -0300

    add covering tests

--- core/src/main/kotlin/br/com/guiabolso/events/validation/TypeValidationHelper.kt
+++ core/src/main/kotlin/br/com/guiabolso/events/validation/TypeValidationHelper.kt
@@ -3,5 +3,5 @@ package br.com.guiabolso.events.validation
 import com.google.gson.JsonNull
 import com.google.gson.JsonObject
 
-fun <T> JsonObject.withCheckedJsonNull(checkedParam: String, block: (jsonObject: JsonObject) -> T): T? =
+fun <T> JsonObject.withCheckedJsonNull(checkedParam: String, block: (jsonObject: JsonObject) -> T?): T? =
         if(this.get(checkedParam) is JsonNull) null else block(this)
---
+++ core/src/test/kotlin/br/com/guiabolso/events/validation/TypeValidationHelper.kt
@@ -0,0 +1,42 @@
+package br.com.guiabolso.events.validation
+
+import br.com.guiabolso.events.json.MapperHolder
+import com.google.gson.JsonObject
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Test
+
+class TypeValidationHelper {
+
+    @Test
+    fun testWithCheckedJsonNullWithNullInput() {
+
+        val jsonObj = MapperHolder.mapper.fromJson("""
+        {
+            "userId": null
+        }
+        """.trimIndent(), JsonObject::class.java)
+
+        val userId = jsonObj.withCheckedJsonNull("userId") {
+            it.getAsJsonPrimitive("userId")
+        }
+
+        assertNull(userId)
+    }
+
+    @Test
+    fun testWithCheckedJsonNullWithValidInput() {
+
+        val identityJsonObj = MapperHolder.mapper.fromJson("""
+            {
+                "userId": 123987
+            }
+        """.trimIndent(), JsonObject::class.java)
+
+        val userId = identityJsonObj.withCheckedJsonNull("userId") {
+            it.getAsJsonPrimitive("userId").asLong
+        }
+
+        assertEquals(123987L, userId)
+    }
+}
