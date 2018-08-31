0671a0eb25522f4c9f61693748c6c53a8862fe2e
chih-chen <Chih.chen1029@gmail.com>
2018-08-31 15:26:03 -0300

    Handling JsonNull to JsonPrimitive (#17)
    * fix JsonNull casting to JsonPrimitive issue
    
    * add covering tests
    
    * minor code rename and refactor

--- core/src/test/kotlin/br/com/guiabolso/events/model/EventErrorTypeTest.kt
+++ core/src/test/kotlin/br/com/guiabolso/events/model/EventErrorTypeTest.kt
@@ -1,8 +1,12 @@
 package br.com.guiabolso.events.model
 
+import br.com.guiabolso.events.EventBuilderForTest
+import br.com.guiabolso.events.json.MapperHolder
 import br.com.guiabolso.events.model.EventErrorType.*
 import br.com.guiabolso.events.model.EventErrorType.Companion.getErrorType
+import com.google.gson.JsonObject
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
 
 class EventErrorTypeTest {
@@ -25,4 +29,55 @@ class EventErrorTypeTest {
         assertEquals("somethingElse", getErrorType("somethingElse").typeName)
     }
 
+    @Test
+    fun testJsonNullUserIdEvent() {
+        val identity = MapperHolder.mapper.fromJson("""
+            {
+                "userId": null
+            }
+        """.trimIndent(), JsonObject::class.java)
+
+        val event = EventBuilderForTest.buildRequestEvent().copy(identity = identity)
+
+        assertNull(event.userId)
+    }
+
+    @Test
+    fun testJsonNullOriginEvent() {
+        val metadata = MapperHolder.mapper.fromJson("""
+            {
+                "origin": null
+            }
+        """.trimIndent(), JsonObject::class.java)
+
+        val event = EventBuilderForTest.buildRequestEvent().copy(metadata = metadata)
+
+        assertNull(event.origin)
+    }
+
+    @Test
+    fun testNotNullUserIdEvent() {
+        val identity = MapperHolder.mapper.fromJson("""
+            {
+                "userId": 123987
+            }
+        """.trimIndent(), JsonObject::class.java)
+
+        val event = EventBuilderForTest.buildRequestEvent().copy(identity = identity)
+
+        assertEquals(123987L, event.userId)
+    }
+
+    @Test
+    fun testNotNullOriginEvent() {
+        val metadata = MapperHolder.mapper.fromJson("""
+            {
+                "origin": "east"
+            }
+        """.trimIndent(), JsonObject::class.java)
+
+        val event = EventBuilderForTest.buildRequestEvent().copy(metadata = metadata)
+
+        assertEquals("east", event.origin)
+    }
 }
