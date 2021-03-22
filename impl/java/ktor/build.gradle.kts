4a8892f1fc812f35f9595717346471145dba642f
Leonardo Colman <leonardo.dev@colman.com.br>
2021-03-22 16:09:40 -0300

    Fix Ktor tests (#80)

--- impl/java/ktor/build.gradle.kts
+++ impl/java/ktor/build.gradle.kts
@@ -8,9 +8,6 @@ dependencies {
     testImplementation("io.ktor:ktor-server-tests:1.5.1")
     
     // Kotest
-    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.2.3") {
-        exclude(group = "org.junit.jupiter")
-        exclude(group = "org.junit.platform")
-    }
-    testImplementation("io.kotest:kotest-assertions-ktor:4.2.3")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.4.3") 
+    testImplementation("io.kotest:kotest-assertions-ktor:4.4.3")
 }
