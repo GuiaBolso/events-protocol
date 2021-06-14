b3d117c06af9893f85011707a9a3a0641ae87917
Neillon <neillon.cesar@guiabolso.com.br>
2021-06-14 16:19:46 -0300

    Adding @SerializedName to solve bug when obfuscating android app

--- impl/java/core/src/main/kotlin/br/com/guiabolso/events/model/EventMessage.kt
+++ impl/java/core/src/main/kotlin/br/com/guiabolso/events/model/EventMessage.kt
@@ -1,6 +1,10 @@
 package br.com.guiabolso.events.model
 
+import com.google.gson.annotations.SerializedName
+
 data class EventMessage(
+    @SerializedName("code")
     val code: String,
+    @SerializedName("parameters")
     val parameters: Map<String, Any?>
 )
