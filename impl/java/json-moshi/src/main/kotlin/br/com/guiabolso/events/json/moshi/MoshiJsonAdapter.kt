4bcf3a6e945decca8a2c771ecbe122396063df8c
Cleidiano Oliveira <cleidiano.souza@guiabolso.com.br>
2022-03-11 05:24:49 -0300

    Wrap moshi exceptions

--- impl/java/json-moshi/src/main/kotlin/br/com/guiabolso/events/json/moshi/MoshiJsonAdapter.kt
+++ impl/java/json-moshi/src/main/kotlin/br/com/guiabolso/events/json/moshi/MoshiJsonAdapter.kt
@@ -1,6 +1,7 @@
 package br.com.guiabolso.events.json.moshi
 
 import br.com.guiabolso.events.json.JsonAdapter
+import br.com.guiabolso.events.json.JsonDataException
 import br.com.guiabolso.events.json.JsonNode
 import br.com.guiabolso.events.json.JsonNull
 import br.com.guiabolso.events.json.moshi.factory.EventProtocolJsonAdapterFactory
@@ -8,18 +9,19 @@ import br.com.guiabolso.events.json.moshi.factory.JsonNodeFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import java.lang.reflect.Type
+import com.squareup.moshi.JsonAdapter as MoshiJsonAdapter
 
-class MoshiJsonAdapter(builder: Moshi.Builder.() -> Moshi.Builder = { this }) : JsonAdapter {
+class MoshiJsonAdapter(builder: Moshi.Builder.() -> Unit = {}) : JsonAdapter {
     private val moshi =
         Moshi.Builder()
             .add(JsonNodeFactory)
             .add(EventProtocolJsonAdapterFactory)
             .addLast(KotlinJsonAdapterFactory())
-            .run(builder)
+            .apply(builder)
             .build()
 
     override fun toJson(any: Any?): String {
-        return nonNullAdapterFor(Any::class.java).toJson(any)
+        return nonNullAdapterFor(Any::class.java).execute { toJson(any) }
     }
 
     override fun toJsonTree(any: Any?): JsonNode {
@@ -28,29 +30,39 @@ class MoshiJsonAdapter(builder: Moshi.Builder.() -> Moshi.Builder = { this }) : 
             is JsonNode -> any
             else -> {
                 val adapter = moshi.adapter(Any::class.java).serializeNulls()
-                val jsonValue = adapter.toJsonValue(any)
-                nonNullAdapterFor(JsonNode::class.java).fromJsonValue(jsonValue)!!
+                val jsonValue = adapter.execute { toJsonValue(any) }
+                nonNullAdapterFor(JsonNode::class.java).execute { fromJsonValue(jsonValue)!! }
             }
         }
     }
 
     override fun <T> fromJson(json: String, clazz: Class<T>): T {
-        return nonNullAdapterFor(clazz).fromJson(json)!!
+        return nonNullAdapterFor(clazz).execute { fromJson(json)!! }
     }
 
     override fun <T> fromJson(json: String, type: Type): T {
         val adapter = nonNullAdapterFor<T>(type)
-        return adapter.fromJson(json)!!
+        return adapter.execute { fromJson(json)!! }
     }
 
     override fun <T> fromJson(jsonNode: JsonNode, type: Type): T {
-        val jsonValue = jsonNodeAdapter().toJsonValue(jsonNode)
-        return nonNullAdapterFor<T>(type).fromJsonValue(jsonValue)!!
+        val jsonValue = jsonNodeAdapter().execute { toJsonValue(jsonNode) }
+
+        return nonNullAdapterFor<T>(type).execute { fromJsonValue(jsonValue)!! }
     }
 
     override fun <T> fromJson(jsonNode: JsonNode, clazz: Class<T>): T {
-        val jsonValue = jsonNodeAdapter().toJsonValue(jsonNode)
-        return nonNullAdapterFor(clazz).fromJsonValue(jsonValue)!!
+        val jsonValue = jsonNodeAdapter().execute { toJsonValue(jsonNode) }
+
+        return nonNullAdapterFor(clazz).execute { fromJsonValue(jsonValue)!! }
+    }
+
+    private fun <T, R> MoshiJsonAdapter<T>.execute(block: MoshiJsonAdapter<T>.() -> R): R {
+        return try {
+            this.block()
+        } catch (e: com.squareup.moshi.JsonDataException) {
+            throw JsonDataException(message = e.message, cause = e)
+        }
     }
 
     private fun jsonNodeAdapter() = moshi.adapter(JsonNode::class.java)
--- impl/java/json-moshi/src/test/kotlin/br/com/guiabolso/events/json/moshi/adapter/MoshiJsonAdapterTest.kt
+++ impl/java/json-moshi/src/test/kotlin/br/com/guiabolso/events/json/moshi/adapter/MoshiJsonAdapterTest.kt
@@ -1,22 +1,25 @@
 package br.com.guiabolso.events.json.moshi.adapter
 
 import br.com.guiabolso.events.json.ArrayNode
+import br.com.guiabolso.events.json.JsonDataException
 import br.com.guiabolso.events.json.JsonNull
-import br.com.guiabolso.events.json.TreeNode
 import br.com.guiabolso.events.json.PrimitiveNode
+import br.com.guiabolso.events.json.TreeNode
 import br.com.guiabolso.events.json.fromJson
 import br.com.guiabolso.events.json.moshi.MoshiJsonAdapter
 import br.com.guiabolso.events.json.moshi.Sample
-import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.throwable.shouldHaveCauseInstanceOf
 import io.kotest.matchers.types.shouldBeSameInstanceAs
 
-class MoshiJsonAdapterTest : AnnotationSpec() {
-    private val jsonString = """
+class MoshiJsonAdapterTest : StringSpec({
+    val jsonString = """
         {"list":[42.42,{"nested":[]},true,"string"],"string":"string","int":42,"boolean":false,"map":{"bla":"bla"}}
         """.trimIndent()
 
-    private val sample = Sample(
+    val sample = Sample(
         int = 42,
         any = null,
         boolean = false,
@@ -25,7 +28,7 @@ class MoshiJsonAdapterTest : AnnotationSpec() {
         list = listOf(42.42, mapOf("nested" to emptyList<Any>()), true, "string")
     )
 
-    private val jsonNode = TreeNode(
+    val jsonNode = TreeNode(
         "int" to PrimitiveNode(42),
         "any" to JsonNull,
         "boolean" to PrimitiveNode(false),
@@ -39,52 +42,47 @@ class MoshiJsonAdapterTest : AnnotationSpec() {
         )
     )
 
-    private val adapter = MoshiJsonAdapter()
+    val adapter = MoshiJsonAdapter()
 
-    @Test
-    fun shouldSerializeObjectSuccessfully() {
+    "should serialize object successfully" {
         adapter.toJson(sample) shouldBe jsonString
     }
 
-    @Test
-    fun shouldSerializeArraySuccessfully() {
+    "should serialize array successfully" {
         adapter.toJson(arrayOf(1, 2, 3, 4)) shouldBe "[1,2,3,4]"
     }
 
-    @Test
-    fun shouldDeserializeSuccessfullyUsingTypeArgument() {
+    "should deserialize successfully using type argument" {
         adapter.fromJson<Sample?>(jsonString) shouldBe sample
     }
 
-    @Test
-    fun shouldDeserializeSuccessfullyUsingClassArgument() {
+    "should deserialize successfully using class argument" {
         adapter.fromJson(jsonString, Sample::class.java) shouldBe sample
     }
 
-    @Test
-    fun shouldDeserializeSuccessfullyUsingJsonNodeAndTypeArgument() {
+    "should deserialize successfully using JsonNodeAndTypeArgument" {
         adapter.fromJson<Sample>(jsonNode) shouldBe sample
     }
 
-    @Test
-    fun shouldDeserializeSuccessfullyUsingJsonNodeAndClassArgument() {
+    "should deserialize successfully using JsonNode and class argument" {
         adapter.fromJson(jsonNode, Sample::class.java) shouldBe sample
     }
 
-    @Test
-    fun shouldReturnJsonNullInstanceOnCallToJsonTreeOnNullObject() {
+    "should return JsonNull instance when call toJsonTree with null " {
         adapter.toJsonTree(null) shouldBeSameInstanceAs JsonNull
     }
 
-    @Test
-    fun shouldReturnsJsonNodeItSelfOnCallToJsonTreeWithJsonNodeArgument() {
-        val jsonNode = TreeNode()
+    "should returns self when call toJsonTree with JsonNode argument" {
         adapter.toJsonTree(jsonNode) shouldBeSameInstanceAs jsonNode
     }
 
-    @Test
-    fun shouldCreateAJsonNodeTree() {
+    "should create a JsonNodeTree" {
         val toJsonTree = adapter.toJsonTree(sample)
         toJsonTree shouldBe jsonNode
     }
-}
+
+    "should wrap and throws moshi json exceptions" {
+        shouldThrow<JsonDataException> { adapter.fromJson<Sample>("{}") }
+            .shouldHaveCauseInstanceOf<com.squareup.moshi.JsonDataException>()
+    }
+})
