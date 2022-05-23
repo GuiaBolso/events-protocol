b6373a2931800fe996f2c679119671810e3f4283
Cleidiano Oliveira <cleidiano.souza@picpay.com>
2022-05-22 21:01:33 -0300

    Improve library ergonomy adding more extension function

--- impl/java/core/src/main/kotlin/br/com/guiabolso/events/json/JsonNodeExtensions.kt
+++ impl/java/core/src/main/kotlin/br/com/guiabolso/events/json/JsonNodeExtensions.kt
@@ -15,3 +15,14 @@ val JsonNode.treeNodeOrNull get() = this as? TreeNode
 private inline fun <reified T> JsonNode.castOrError(): T {
     return this as? T ?: throw IllegalArgumentException("JsonNode is not a ${T::class.java}")
 }
+
+val JsonNode.string get() = primitiveNode.value
+val JsonNode.stringOrNull get() = primitiveNode.stringOrNull
+val JsonNode.boolean get() = primitiveNode.boolean
+val JsonNode.booleanOrNull get() = primitiveNode.booleanOrNull
+val JsonNode.int get() = primitiveNode.int
+val JsonNode.intOrNull get() = primitiveNode.intOrNull
+val JsonNode.long get() = primitiveNode.long
+val JsonNode.longOrNull get() = primitiveNode.longOrNull
+val JsonNode.double get() = primitiveNode.double
+val JsonNode.doubleOrNull get() = primitiveNode.doubleOrNull
--- impl/java/core/src/main/kotlin/br/com/guiabolso/events/json/PrimitiveNodeExtensions.kt
+++ impl/java/core/src/main/kotlin/br/com/guiabolso/events/json/PrimitiveNodeExtensions.kt
@@ -23,3 +23,7 @@ val PrimitiveNode.stringOrNull get() = if (this is JsonNull) null else value
 fun PrimitiveNode(value: String?) = if (value == null) JsonNull else JsonLiteral(value)
 fun PrimitiveNode(value: Number?) = if (value == null) JsonNull else JsonLiteral(value)
 fun PrimitiveNode(value: Boolean?) = if (value == null) JsonNull else JsonLiteral(value)
+
+fun String?.toPrimitiveNode() = PrimitiveNode(this)
+fun Boolean?.toPrimitiveNode() = PrimitiveNode(this)
+fun Number?.toPrimitiveNode() = PrimitiveNode(this)
