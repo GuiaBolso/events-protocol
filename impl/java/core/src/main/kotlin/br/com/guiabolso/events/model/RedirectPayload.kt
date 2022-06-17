3fc41581868d7f232a9adae0a74e468f66c1af8e
Cleidiano Oliveira <cleidiano.souza@picpay.com>
2022-06-17 13:07:33 -0300

    Use JsonNode instead

--- impl/java/core/src/main/kotlin/br/com/guiabolso/events/model/RedirectPayload.kt
+++ impl/java/core/src/main/kotlin/br/com/guiabolso/events/model/RedirectPayload.kt
@@ -1,6 +1,8 @@
 package br.com.guiabolso.events.model
 
+import br.com.guiabolso.events.json.JsonNode
+
 data class RedirectPayload(
     val url: String,
-    val queryParameters: Map<String, Any> = emptyMap()
+    val queryParameters: Map<String, JsonNode> = emptyMap()
 )
