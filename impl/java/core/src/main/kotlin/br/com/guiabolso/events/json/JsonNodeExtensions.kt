ab83a527e92d301e1d7676a629c77565d2cb4f82
Cleidiano Oliveira <cleidiano.souza@picpay.com>
2022-07-07 11:30:45 -0300

    string extension function throws on JsonNull

--- impl/java/core/src/main/kotlin/br/com/guiabolso/events/json/JsonNodeExtensions.kt
+++ impl/java/core/src/main/kotlin/br/com/guiabolso/events/json/JsonNodeExtensions.kt
@@ -16,13 +16,17 @@ private inline fun <reified T> JsonNode.castOrError(): T {
     return this as? T ?: throw IllegalArgumentException("JsonNode is not a ${T::class.java}")
 }
 
-val JsonNode.string get() = primitiveNode.value
+val JsonNode.string get() = stringOrNull!!
 val JsonNode.stringOrNull get() = primitiveNodeOrNull?.run { if (this is JsonNull) null else value }
+
 val JsonNode.boolean get() = primitiveNode.value.toBooleanStrict()
 val JsonNode.booleanOrNull get() = primitiveNodeOrNull?.value?.toBooleanStrictOrNull()
+
 val JsonNode.int get() = primitiveNode.value.toInt()
 val JsonNode.intOrNull get() = primitiveNodeOrNull?.value?.toIntOrNull()
+
 val JsonNode.long get() = primitiveNode.value.toLong()
 val JsonNode.longOrNull get() = primitiveNodeOrNull?.value?.toLongOrNull()
+
 val JsonNode.double get() = primitiveNode.value.toDouble()
 val JsonNode.doubleOrNull get() = primitiveNodeOrNull?.value?.toDoubleOrNull()
