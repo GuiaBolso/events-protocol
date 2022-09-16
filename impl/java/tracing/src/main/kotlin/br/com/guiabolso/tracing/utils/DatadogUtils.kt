ad1b007be2ea4004b097d125a3245f9ad9086efd
Bruno Ortiz <brunortiz11@gmail.com>
2022-09-16 17:52:35 -0300

    Fix notify root error on V6 branch (#100)

--- impl/java/tracing/src/main/kotlin/br/com/guiabolso/tracing/utils/DatadogUtils.kt
+++ impl/java/tracing/src/main/kotlin/br/com/guiabolso/tracing/utils/DatadogUtils.kt
@@ -5,6 +5,7 @@ import datadog.trace.api.DDTags.ERROR_MSG
 import datadog.trace.api.DDTags.ERROR_STACK
 import datadog.trace.api.DDTags.ERROR_TYPE
 import datadog.trace.api.DDTags.SPAN_TYPE
+import datadog.trace.api.interceptor.MutableSpan
 import io.opentracing.Span
 import io.opentracing.tag.Tags
 import io.opentracing.util.GlobalTracer
@@ -73,7 +74,22 @@ object DatadogUtils {
     }
 
     @JvmStatic
+    fun notifyError(span: MutableSpan, exception: Throwable, expected: Boolean) {
+        span.setTag(Tags.ERROR.key, !expected)
+        span.setTag(ERROR_MSG, exception.message ?: "Empty message")
+        span.setTag(ERROR_TYPE, exception.javaClass.name)
+        span.setTag(ERROR_STACK, ExceptionUtils.getStackTrace(exception))
+    }
+
+    @JvmStatic
     fun notifyError(span: Span, message: String, params: Map<String, String?>, expected: Boolean) {
+        span.setTag(Tags.ERROR.key, !expected)
+        span.setTag(ERROR_MSG, message)
+        params.forEach { entry -> span.setTag(entry.key, entry.value) }
+    }
+
+    @JvmStatic
+    fun notifyError(span: MutableSpan, message: String, params: Map<String, String?>, expected: Boolean) {
         span.setTag(Tags.ERROR.key, !expected)
         span.setTag(ERROR_MSG, message)
         params.forEach { entry -> span.setTag(entry.key, entry.value) }
