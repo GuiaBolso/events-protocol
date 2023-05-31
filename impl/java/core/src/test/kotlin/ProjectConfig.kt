9e989ad34892e3d04daa049d92bc7e5d5a81aed1
gb-jairo <jairo.santos@guiabolso.com.br>
2023-05-31 16:14:31 -0300

    log pra mostrar qual adapter foi usado nos testes

--- impl/java/core/src/test/kotlin/ProjectConfig.kt
+++ impl/java/core/src/test/kotlin/ProjectConfig.kt
@@ -5,7 +5,11 @@ import br.com.guiabolso.events.json.moshi.MoshiJsonAdapter
 import com.fasterxml.jackson.module.kotlin.kotlinModule
 import io.kotest.core.config.AbstractProjectConfig
 import io.kotest.core.extensions.Extension
+import io.kotest.core.listeners.AfterEachListener
 import io.kotest.core.listeners.BeforeProjectListener
+import io.kotest.core.test.TestCase
+import io.kotest.core.test.TestResult
+import org.slf4j.LoggerFactory
 
 object ProjectConfig : AbstractProjectConfig() {
 
@@ -14,12 +18,19 @@ object ProjectConfig : AbstractProjectConfig() {
     }
 }
 
-object MapperHolderInitializer : BeforeProjectListener {
+object MapperHolderInitializer : BeforeProjectListener, AfterEachListener {
+
+    private val logger = LoggerFactory.getLogger(MapperHolderInitializer::class.java)
+
     override suspend fun beforeProject() {
         MapperHolder.mapper = listOf(
             MoshiJsonAdapter(),
             GsonJsonAdapter(),
             Jackson2JsonAdapter { addModule(kotlinModule()) },
         ).random()
+    }
+
+    override suspend fun afterEach(testCase: TestCase, result: TestResult) {
+        logger.info("\nNOTA: Teste executado com Mapper: ${MapperHolder.mapper.javaClass.simpleName}")
     }
 }
--- impl/java/core/src/test/kotlin/br/com/guiabolso/events/MapperHolderSetup.kt
+++ impl/java/core/src/test/kotlin/br/com/guiabolso/events/MapperHolderSetup.kt
@@ -7,15 +7,25 @@ import br.com.guiabolso.events.json.moshi.MoshiJsonAdapter
 import com.fasterxml.jackson.module.kotlin.kotlinModule
 import org.junit.jupiter.api.extension.BeforeAllCallback
 import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.platform.engine.TestExecutionResult
+import org.junit.platform.launcher.TestExecutionListener
+import org.junit.platform.launcher.TestIdentifier
+import org.slf4j.LoggerFactory
 
-class MapperHolderSetup : BeforeAllCallback {
+class MapperHolderSetup : BeforeAllCallback, TestExecutionListener {
 
-    override fun beforeAll(context: ExtensionContext?) {
+    private val logger = LoggerFactory.getLogger(MapperHolderSetup::class.java)
+
+    override fun beforeAll(context: ExtensionContext) {
 
         MapperHolder.mapper = listOf(
             MoshiJsonAdapter(),
             GsonJsonAdapter(),
             Jackson2JsonAdapter { addModule(kotlinModule()) },
         ).random()
+    }
+
+    override fun executionFinished(testIdentifier: TestIdentifier, testExecutionResult: TestExecutionResult) {
+        logger.info("\nNOTA: Teste executado com Mapper: ${MapperHolder.mapper.javaClass.simpleName}")
     }
 }
